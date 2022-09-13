0ed1fd9a3e6b473d2aa3154629bef0e5f1e3cd43
Ben A <benjamin.anoufa@gmail.com>
2022-09-13 19:28:19 +0200

    Security on prompt

--- scripts/encryptKey.js
+++ scripts/encryptKey.js
@@ -1,25 +1,72 @@
-const {ethers} = require("hardhat");
+const { ethers } = require("hardhat");
 const fs = require("fs-extra");
-const path = require('path');
+const path = require("path");
+const prompt = require("prompt");
 require("dotenv").config();
 
 async function main() {
-  const wallet = new ethers.Wallet(process.env.SECRET_KEY);
-  const encryptedJsonKey = await wallet.encrypt(process.env.PASSWORD, process.env.SECRET_KEY);
+    let publicAddress; 
 
-  const keysDir = path.join(__dirname, '..', 'encryptedKeys');
+    const promptSchema = {
+        properties: {
+            privateKey: {
+                before: function (v) {
+                    try {
+                        publicAddress = ethers.utils.computeAddress(v);
+                        if(ethers.utils.isAddress(publicAddress))
+                        return v;
+                    } catch (e) {
+                        throw new Error("Invalid Private Key");
+                    }
+                },
+                message: "Enter your ETH Private Key",
+                required: true,
+                hidden: true,
+            },
+            password: {
+                message: "Choose a strong password",
+                hidden: true,
+                required: true,
+            },
+        },
+    };
 
-  if (!fs.existsSync(keysDir)) {
-    fs.mkdirSync(keysDir);
-  }
+    const promptResult = await new Promise((resolve, reject) => {
+        prompt.start();
+        prompt.get(promptSchema, function (err, result) {
+            resolve(result);
+            reject(err);
+        });
+    });
 
-  fs.writeFileSync(
-    path.join(keysDir, '.encryptedKey.json'), encryptedJsonKey);
+    const SECRET_KEY = promptResult.privateKey;
+    const PASSWORD = promptResult.password;
+
+    const wallet = new ethers.Wallet(SECRET_KEY);
+    const encryptedJsonKey = await wallet.encrypt(PASSWORD, SECRET_KEY);
+
+    const keysDir = path.join(__dirname, "..", "encryptedKeys");
+
+    if (!fs.existsSync(keysDir)) {
+        fs.mkdirSync(keysDir);
+    }
+
+    const keysPath = path.join(keysDir, ".encryptedKey.json");
+
+    fs.writeFileSync(
+        keysPath,
+        encryptedJsonKey
+    );
+    
+    console.log('');
+    console.log(`🌈 - Success your Private key has been encrypted at: ${keysPath}`);
+    console.log(`👤 - For the following public addres: ${publicAddress}`);
+    console.log('');
 }
 
 main().then(() =>
-  process.exit(0).catch(err => {
-    console.error(err);
-    process.exit(1);
-  })
+    process.exit(0).catch((err) => {
+        console.error(err);
+        process.exit(1);
+    })
 );
